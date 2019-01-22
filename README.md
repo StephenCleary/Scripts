7e4cef9299de2691cd3c4f25a574648590d4c339
Stephen Cleary <StephenCleary@users.noreply.github.com>
2019-01-21 22:17:07 -0500

    Add vs.ps1.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Scripts
 Misc single-file scripts and utilities
+
+## vs.ps1
+PowerShell script to pull in Visual Studio environment variables.
---
+++ vs.ps1
@@ -0,0 +1,8 @@
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools"
+cmd /c "VsDevCmd.bat&set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+  }
+}
+popd
